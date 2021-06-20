04dabf6ad7ad9bcdfba5a82b98723c637aec35ea
Udit Gupta <uditgupta0912@gmail.com>
2021-06-20 01:17:42 +0100

    Add files via upload

--- Code/Inputfile/GetAdsorbateGraph3.py
+++ Code/Inputfile/GetAdsorbateGraph3.py
@@ -1,16 +1,27 @@
 import os
 import networkx as nx
-from RuleGeneration.Output import adsorbate
+from RuleGeneration import Output as O
+#from RuleGeneration.Output import adsorbate
+#from RuleGeneration.ReactionRule import reactionrule
 from RuleGeneration.SmilesRelated import FindAllAdsorbateGraphsOfLengthN
 from RuleGeneration.ReactionRule import *
 from rdkit import Chem
 
 import numpy as np
 from rdkit.Chem.rdchem import RWMol
+from rdkit.Chem import rdFMCS
+
+from rdkit import DataStructs
+from rdkit.Chem.Fingerprints import FingerprintMols
 
 #Process published reactions
-f = open('ReactionRules.txt','w')
-#f.write('Reaction Rules Generated\n')
+f = open('AllFiles/ReactionRules.txt','w')
+db = open('DatabaseEntries.json','w')
+sf = open('SmilesForReactions.txt','w')
+
+
+sf.close()
+db.close()
 f.close()
 file = open("PublishedReactions.txt","r")
 reactioncount = 0
@@ -18,7 +29,14 @@ InputFolderPath = '.\\AllFiles\\'
 SurfaceAtomSymbols = ['Pt']
 LatticePattern = ['[{Pt3}]12[{Pt4}][{Pt2}]1[{Pt1}]2']
 for line in file:
-    reactant_molecules = []
+    outputfile = open('AllFiles/ReactionRules.txt','a')
+    smilesfile = open('SmilesForReactions.txt','a')
+#    outputjsonfile = open('DatabaseEntries.json','a')
+#    print('{', end="", flush=True, file=outputjsonfile)
+    reactant_molecules = list()
+    product_molecules = list()
+    adsorbate_molecules_reac = list()
+    adsorbate_molecules_prod = list()
     mole = 0
     adj_mat = 0
     mol_formula = 0
@@ -27,102 +45,174 @@ for line in file:
     reactantside = reaction[0]
     productside = reaction[1]
     reactants = reactantside.split(" + ")
-    products = productside.split(" + ")
+    productside_trim = productside.split("\n")
+    productside_trim = productside_trim[0]
+    products = productside_trim.split(" + ")
     print(len(reactants), len(products))
-    outputfile = open('ReactionRules.txt','a')
-    print('rule R'+str(reactioncount)+'{', file=outputfile)
+    print(reactants)
+    print(products)
+    
+    
+#    print('rule R'+str(reactioncount)+'{', file=outputfile)
     for i in range(len(reactants)):
-        if i <= 1:
-            print('\treactant r'+str(i+1)+'{', file=outputfile)
-            outputfile.close()
-            filepath = InputFolderPath + reactants[i] + '\\CONTCAR'
-            mole, adj_mat, mol_formula = adsorbate.LoadByCovalentRadius(filepath, SurfaceAtomSymbols = SurfaceAtomSymbols)
+        filepath = InputFolderPath + reactants[i] + '\\CONTCAR'
+        mole, adj_mat, mol_formula = O.adsorbate.LoadByCovalentRadius(filepath, SurfaceAtomSymbols = SurfaceAtomSymbols)
+#        if i <= 1:
+#            print('\treactant r'+str(i+1)+'{', file=outputfile)
+#            outputfile.close()
+#            filepath = InputFolderPath + reactants[i] + '\\CONTCAR'
 #            for atoms in mole.RdkitMol.GetAtoms():
 #                print(atoms.GetProp('Label'))
-            reactant_molecules.append(mole)
-            outputfile = open('ReactionRules.txt','a')
-            print('Hydrogens:', np.int(mole.NumofH))
-            print(mole.OAI.__len__())
-            if mole.OAI.__len__() == 0:
-                print('\t\t'+SurfaceAtomSymbols[0]+'1 labeled p5 {! connected to H with any bond}', file=outputfile)
-                print('\t\t'+SurfaceAtomSymbols[0]+'2 labeled p6 single bond to p5', file=outputfile)
-                print('\t\t'+SurfaceAtomSymbols[0]+'3 labeled p7 single bond to p6', file=outputfile)
-                print('\t\t'+SurfaceAtomSymbols[0]+'4 labeled p8 single bond to p7', file=outputfile)
-            print (FindAllAdsorbateGraphsOfLengthN(mole.RdkitMol,SurfaceAtomSymbols,1))
-            print('\t}', file=outputfile)
-        else: #generally the third reactant is a duplicate and generally an empty site
-            print('\treactant r'+str(i+1)+' duplicates r'+str(i)+'(p5 => q1, p6 => q2, p7 => q3, p8 => q4)', file=outputfile)
-            
-    
-    
-    
-    print('\tconstraints{', file=outputfile)
+        reactant_molecules.append(mole.RdkitMol)
+        adsorbate_molecules_reac.append(mole)
+        assert isinstance(mole,O.adsorbate)
+#            outputfile = open('AllFiles/ReactionRules.txt','a')
+#        print('Hydrogens:', np.int(mole.NumofH))
+#        print(mole.OAI.__len__())
+#        if mole.OAI.__len__() == 0:
+#                print('\t\t'+SurfaceAtomSymbols[0]+'1 labeled p5 {! connected to H with any bond}', file=outputfile)
+#                print('\t\t'+SurfaceAtomSymbols[0]+'2 labeled p6 single bond to p5', file=outputfile)
+#                print('\t\t'+SurfaceAtomSymbols[0]+'3 labeled p7 single bond to p6', file=outputfile)
+#                print('\t\t'+SurfaceAtomSymbols[0]+'4 labeled p8 single bond to p7', file=outputfile)
+#        print (FindAllAdsorbateGraphsOfLengthN(mole.RdkitMol,SurfaceAtomSymbols,1))
+#            print('\t}', file=outputfile)
+#    else: #generally the third reactant is a duplicate and generally an empty site
+#            print('\treactant r'+str(i+1)+' duplicates r'+str(i)+'(p5 => q1, p6 => q2, p7 => q3, p8 => q4)', file=outputfile)
+#     for i in range(len(products)):
+#    print('\tconstraints{', file=outputfile)
+    reactantstring = list()
     for i in range(len(reactant_molecules)):
-        NumofHeavyAtoms = reactant_molecules[i].NumofC + reactant_molecules[i].NumofO + reactant_molecules[i].NumofN
-        print('\tr'+str(i+1)+'.size = '+str(NumofHeavyAtoms), file=outputfile),
-    print('\t}', file=outputfile)
+        assert isinstance(reactant_molecules[i],Chem.Mol)
+        assert isinstance(adsorbate_molecules_reac[i],O.adsorbate)
+        NumofHeavyAtoms = adsorbate_molecules_reac[i].NumofC + adsorbate_molecules_reac[i].NumofO + adsorbate_molecules_reac[i].NumofN
+        NumofNonHeavyAtoms = adsorbate_molecules_reac[i].NumofH
+        NumnofPtAtoms = adsorbate_molecules_reac[i].NumofPt
+        print('H: ',NumofHeavyAtoms,' NH: ',NumofNonHeavyAtoms,' Pt: ',NumnofPtAtoms)
+        smiles = Chem.MolToSmiles(reactant_molecules[i])
+        print(smiles)
+        reactantstring.append(smiles)
+        print('\tr'+str(i+1)+'.size = '+str(NumofHeavyAtoms+NumnofPtAtoms), file=outputfile),
     
-    print('}\n', file=outputfile)
-    outputfile.close()
+    #Combining reactant side smile strings
+    s = '.'
+    reactantstring = s.join(reactantstring)
+    print('\t}', file=outputfile)
 
+    for i in range(len(products)):
+        filepath = InputFolderPath + products[i] + '\\CONTCAR'
+        mole, adj_mat, mol_formula = O.adsorbate.LoadByCovalentRadius(filepath, SurfaceAtomSymbols = SurfaceAtomSymbols)
+        product_molecules.append(mole.RdkitMol)
+        adsorbate_molecules_prod.append(mole)
+        
+    productstring = list()
+    for i in range(len(product_molecules)):
+        assert isinstance(product_molecules[i],Chem.Mol)
+        assert isinstance(adsorbate_molecules_prod[i],O.adsorbate)
+        NumofHeavyAtoms = adsorbate_molecules_prod[i].NumofC + adsorbate_molecules_prod[i].NumofO + adsorbate_molecules_prod[i].NumofN
+        NumofNonHeavyAtoms = adsorbate_molecules_prod[i].NumofH
+        NumnofPtAtoms = adsorbate_molecules_prod[i].NumofPt
+        print('H: ',NumofHeavyAtoms,' NH: ',NumofNonHeavyAtoms,' Pt: ',NumnofPtAtoms)
+        smiles = Chem.MolToSmiles(product_molecules[i])
+        print(smiles)
+        productstring.append(smiles)
+        print('\tr'+str(i+1)+'.size = '+str(NumofHeavyAtoms+NumnofPtAtoms), file=outputfile),
+    
+    #Combining product side smile strings
+    s = '.'
+    productstring = s.join(productstring)
+    
+    #Combining reaction smile strings
+    reactionstring = list()
+    s= '>>'
+    reactionstring.append(reactantstring)
+    reactionstring.append(productstring)
+    reactionstring = s.join(reactionstring)
+    print(reactionstring, file=smilesfile)
+    
+    RRreturn = reactionrule.GenerateReactionRule(adsorbate_molecules_reac,adsorbate_molecules_prod,SurfaceAtomSymbols)
 
-#AllMolecules = []
-#MoleculeNames = []
-#for datum in os.listdir(InputFolderPath):
-#    #print(datum)
-#    #datum does not capture the molecule correctly
-#    #better to save molecule name based on connectivity and SMILES string
-#    MoleculeNames.append(datum)
-#    fpath = InputFolderPath + datum + '\\CONTCAR'
-#    mole, adj_mat, mol_formula = adsorbate.LoadByCovalentRadius(fpath, SurfaceAtomSymbols = SurfaceAtomSymbols)
-#    AllMolecules.append(mol_formula)
-#    print FindAllAdsorbateGraphsOfLengthN(mole.RdkitMol,SurfaceAtomSymbols,1)
-#    print(adj_mat)
-#    
-################################ USER INPUT ###################################
-#InputPath = '..\\Example\\Input\\'
+    reac_mol = Chem.Mol()
+    reac_mol = Chem.RWMol(reac_mol)
+    prod_mol = Chem.Mol()
+    prod_mol = Chem.RWMol(prod_mol)
+#    reac_mol = 0
+#    prod_mol = 0
+    ps = Chem.SmilesParserParams()
+    ps.removeHs = False
+    ps.sanitize = False
+    
+    print('Out Here')
+    
+    for i in range(len(adsorbate_molecules_reac)):
+        reac = adsorbate_molecules_reac[i]
+        assert isinstance(adsorbate_molecules_reac[i],O.adsorbate)
+        if len(reac.OAI) > 0:
+            #Main species to look at
+            print('reactant here')
+            reac_smiles = Chem.MolToSmiles(reac.RdkitMol)
+            reac_mol = Chem.MolFromSmiles(reac_smiles,ps)
+            break
+            
+    for i in range(len(adsorbate_molecules_prod)):
+        prod = adsorbate_molecules_prod[i]
+        assert isinstance(adsorbate_molecules_prod[i],O.adsorbate)
+        if len(prod.OAI) > 0:
+            print('product here')
+            #Main species to look at
+            prod_smiles = Chem.MolToSmiles(prod.RdkitMol)
+            prod_mol = Chem.MolFromSmiles(prod_smiles,ps)
+            break
+            
+    print(Chem.MolToSmiles(reac_mol),Chem.MolToSmiles(prod_mol))
+    mols = [reac_mol,prod_mol]
+    maxcomsub = rdFMCS.FindMCS(mols)
+    print(maxcomsub.smartsString)
 
-################################ USER INPUT ###################################
-# Get SMILES
-#for datum in os.listdir(InputPath):
-#    fpath = InputPath + datum + '\\CONTCAR'
-#    # Convert atomic coordinates (VASP) to molecular graph.
-#    mole, adj_mat = adsorbate.LoadByCovalentRadius(fpath, SurfaceAtomSymbols = SurfaceAtomSymbols)
-#    # Mine adsorbate graphs and print
-#    print FindAllAdsorbateGraphsOfLengthN(mole.RdkitMol,SurfaceAtomSymbols,1)
-#    print(adj_mat)
+reactionrule.printdict()    
+smilesfile.close()
 
-#InputPath_R = '.\\REACTANT\\'
-#InputPath_P = '.\\PRODUCT\\'
+mol1 = Chem.MolFromSmiles('[H][H]',ps)
+mol2 = Chem.MolFromSmiles('[H]~[Pt]',ps)
+mols = [mol1, mol2]
+res1 = rdFMCS.FindMCS(mols) 
+res2 = rdFMCS.FindMCS(mols,atomCompare=rdFMCS.AtomCompare.CompareElements)
+print(res1.numAtoms,res1.numBonds,res1.smartsString)
+print(res2.numAtoms,res2.numBonds,res2.smartsString)
+
+m1 = Chem.MolFromSmiles('[H]C([H])([H])[H]',ps)
+m2 = Chem.MolFromSmiles('[H][C]([H])([H])~[Pt]',ps)
+ms = [m1,m2]
+r1 = rdFMCS.FindMCS(ms) 
+r2 = rdFMCS.FindMCS(ms,atomCompare=rdFMCS.AtomCompare.CompareElements,bondCompare=rdFMCS.BondCompare.CompareOrder)
+m3 = Chem.MolFromSmarts(r1.smartsString)
+m4 = Chem.MolFromSmarts(r2.smartsString)
+print(r1.numAtoms,r1.numBonds,r1.smartsString)
+print(r2.numAtoms,r2.numBonds,r2.smartsString)
+bis1 = m1.GetSubstructMatch(m3)
+bis2 = m1.GetSubstructMatch(m4)
+print('bis1: ',bis1)
+print('bis2: ',bis2)
+#mols = [m1, m2] 
+##bis = m1.GetSubstructMatches(m2)
+##print('bis: ',bis)
+##fps = [FingerprintMols.FingerprintMol(x) for x in mols]
+##print('sim: ',DataStructs.FingerprintSimilarity(fps[0],fps[1]))
+##
+###print(DataStructs.FingerprintSimilarity(m1,m2))
+##print(m1.HasSubstructMatch(m2))
 #
-#mole_r = ' '
-#mole_p = ' '
-#reactant_atoms_count = 0
-#product_atoms_count = 0
-#
-#for datum_r in os.listdir(InputPath_R):
-#    fpath = InputPath_R + datum_r + '\\CONTCAR'
-#    mole_r, adj_mat_r, mol_formula_r = adsorbate.LoadByCovalentRadius(fpath, SurfaceAtomSymbols = SurfaceAtomSymbols)
-#    print (FindAllAdsorbateGraphsOfLengthN(mole_r.RdkitMol,SurfaceAtomSymbols,1))
-##    print(adj_mat_r)
-##    print(adj_mat_r.shape[0])
-#    reactant_atoms_count += adj_mat_r.shape[0]
 #    
-#print('Done with reactant')
-#print(reactant_atoms_count)
+#    print('}\n', file=outputfile)
+#    outputfile.close()
+    
+#    if len(reactants) == 1:
+#        print('"reaction":"unimolecular"', file=outputjsonfile)
+#    elif len(reactants) == 2:
+#        print('"reaction":"bimolecular"', file=outputjsonfile)
+#    else:
+#        print('"reaction":"trimolecular"', file=outputjsonfile)
 #    
-#for datum_p in os.listdir(InputPath_P):
-#    fpath = InputPath_P + datum_p + '\\CONTCAR'
-#    mole_p, adj_mat_p, mol_formula_p = adsorbate.LoadByCovalentRadius(fpath, SurfaceAtomSymbols = SurfaceAtomSymbols)
-#    print (FindAllAdsorbateGraphsOfLengthN(mole_p.RdkitMol,SurfaceAtomSymbols,1))
-##    print(adj_mat_p, adj_mat_p.shape[0])
-#    product_atoms_count += adj_mat_p.shape[0]
-#    
-#print('Done with product')
-#print(product_atoms_count)
-
-#Specify reaction manually
-
+#    print('}', file=outputjsonfile)
 
 #m = Chem.MolFromSmiles('CCC')
 #patt1 = Chem.MolFromSmiles('CC')
